b2fa4966a19edd4e23d05dd4f66ad3de80c42f31
Dmitry Zaguzin <cindersstone@gmail.com>
2018-12-22 18:09:23 +0800

    Изменяются цвета кнопок в зависимости от текста

--- create_key_board.py
+++ create_key_board.py
@@ -15,7 +15,13 @@ def new_key_board(labels_for_keyboard):
     for i in labels_for_keyboard:
         new_row = []
         for j in i:
-            new_row.append(get_button(label = j, color = "positive"))
+            if j.isdigit():
+                new_row.append(get_button(label = j, color = "positive"))
+            elif j == 'Отмена' or j == 'Назад':
+                new_row.append(get_button(label = j, color = "negative"))
+            else:
+                new_row.append(get_button(label = j, color = "primary"))
+
         buttons_row.append(new_row)
             
     keyboard = {
