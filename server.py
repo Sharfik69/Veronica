6a69b53e72e6904f417da4028c8a763df5f937fd
Dmitry Zaguzin <cindersstone@gmail.com>
2018-12-22 20:12:34 +0800

    Генерация кнопок на каждое событие

--- server.py
+++ server.py
@@ -5,8 +5,9 @@
 import vk_api.vk_api
 import add_table
 import random
-from create_key_board import new_key_board
 
+from collections import defaultdict
+from create_key_board import new_key_board
 from vk_api import VkUpload
 from collections import defaultdict
 from vk_api.longpoll import VkLongPoll, VkEventType
@@ -17,6 +18,7 @@ class Server:
         self.vk = vk_api.VkApi(token = api_token)
         self.longpoll = VkLongPoll(self.vk)
         self.vk_api = self.vk.get_api()
+        self.step = defaultdict(int)
     def send_msg(self, send_id, message, key_board):
         self.vk_api.messages.send(peer_id = send_id,
                                   message = message,
@@ -30,6 +32,44 @@ class Server:
         test_list.LoadTable()
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                if self.step[event.user_id] == 0:
+                    self.send_msg(event.user_id, 'Добро пожаловать, выбери на сколько человек', 
+                                [['Столик на двоих'], ['Столик на троих'], ['Столик на четверых'], ['Столик на большую компанию']])
+                    self.step[event.user_id] = 1
+                elif self.step[event.user_id] == 1:
+
+                    def print_list_table(cnt_person):
+                        check_table = False
+                        for i in range(test_list.FreeTable()):
+                            s = test_list.AboutTable(i, cnt_person)
+                            if s != 'about':
+                                check_table = True
+                                self.send_msg(event.user_id, s, test_list.List_of_free_table(cnt_person))
+                        return check_table
+
+                    if event.text == 'Столик на двоих':
+                        if print_list_table(2) == False:
+                            self.send_msg(event.user_id, 'На данный момент все столики на двоих заняты', [['Назад']])
+                    elif event.text == 'Столик на троих':
+                        if print_list_table(3) == False:
+                            self.send_msg(event.user_id, 'На данный момент все столики на троих заняты', [['Назад']])
+                    elif event.text == 'Столик на четверых':
+                        if print_list_table(4) == False:
+                            self.send_msg(event.user_id, 'На данный момент все столики на четверых заняты', [['Назад']])
+                    elif event.text == 'Столик на большую компанию':
+                        if print_list_table(5) == False:
+                            self.send_msg(event.user_id, 'На данный момент все столики на большую компанию заняты', [['Назад']])
+                    elif event.text == 'Назад':
+                        self.step[event.user_id] = 1
+                        self.send_msg(event.user_id, 'Добро пожаловать, выбери на сколько человек', 
+                                [['Столик на двоих'], ['Столик на троих'], ['Столик на четверых'], ['Столик на большую компанию']])
+                    
+
+
+
+
+
+                '''
                 if event.text == 'Столы':
                     for i in range(test_list.FreeTable()):
                         s = test_list.AboutTable(i, 2)
@@ -41,6 +81,7 @@ class Server:
                         self.send_msg(event.user_id, 'Стол забронирован', [['Ok']])
                     else:
                         self.send_msg(event.user_id, 'Стол занят', [['Назад']])
+                '''
 
 
     def test(self):
